f9d0c37972d929545ad7c439767ffd15ed630b56
Nikolay Karasev <nomotorolla@gmail.com>
2023-08-25 15:01:59 +0300

    дано решение для первогй задачи из домашнего задания к seminaru_3

--- HomeWork/Seminar_3/task01.py
+++ HomeWork/Seminar_3/task01.py
@@ -4,4 +4,17 @@
 # k = 3
 # 1
 
+import random
 
+# ls = int(input("Введите длину списка: "))
+list_1 = [random.randint(1, 20) for _ in range(15)]
+k = 3
+n = 0
+for i in range(len(list_1)):
+    if i == k:
+        n += 1
+        i += 1
+
+# print(str(list_1).strip('[]'))
+print(list_1)
+print(n)
